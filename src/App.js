5a97ffcf4264596ef376933ca60be45c52fe8dc0
Fahrul Rozi <mfahrul.rozi@izeno.com>
2024-03-20 14:17:52 +0700

    Add new menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
 import Datatable from "./Component/Datatable";
 import AddDataTable from "./Component/AddData";
+import Sidebar from './Component/Sidebar';
+import HomePage from './Component/Homepage';
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Sidebar />
       <Routes>
-        <Route path="/" element={<Datatable />} />
+      <Route path="/" element={<HomePage />} />
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/Preview" element={<Datatable />} />
         <Route path="/Add" element={<AddDataTable />} />
       </Routes>
     </BrowserRouter>
